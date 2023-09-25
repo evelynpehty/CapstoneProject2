6cd99fc2b10fa13413a8ddf40faf40955af5698c
Justin Lee <jlcw99@gmail.com>
2023-09-25 11:57:39 +0800

    error solved

--- capstone_project/src/main/java/com/pages/CreateAccount.java
+++ capstone_project/src/main/java/com/pages/CreateAccount.java
@@ -14,12 +14,7 @@ import com.validations.MenuChoices;
 public class CreateAccount {
 
     public static void show(Scanner scanner, String nric) {
-        PreparedStatement pstmt;
-        ResultSet resultSet;
-        int choice;
         boolean isExit = false;
-        String sql;
-        String acctType = "";
 
         while (!isExit) {
             System.out.println("Please select from the following options");
@@ -28,6 +23,13 @@ public class CreateAccount {
             System.out.println("2. Checking Account");
             System.out.println("3. Fixed Deposit Account");
             System.out.println("4. Back");
+
+            PreparedStatement pstmt;
+            ResultSet resultSet;
+            int choice;
+            String sql;
+            String acctType = "";
+            Boolean createAcc = true;
 
             choice = MenuChoices.getUserChoice(scanner, 4);
 
@@ -44,53 +46,57 @@ public class CreateAccount {
                 case 4:
                     isExit = MenuChoices.yesnoConfirmation(scanner,
                             "Are you sure you want to exit the application? Y/N: ");
+                    createAcc = false;
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-            try {
-                System.out.println(acctType);
-                sql = "SELECT * FROM ACCOUNT WHERE nric = upper(?) AND ACCOUNT_TYPE =?";
-                pstmt = GetConn.getPreparedStatement(sql);
-                pstmt.setString(1, nric);
-                pstmt.setString(2, acctType);
-                resultSet = pstmt.executeQuery();
-
-                if (!resultSet.next()) {
-                    sql = "INSERT INTO ACCOUNT(ACCOUNT_ID, NRIC, ACCOUNT_BALANCE, ACCOUNT_CREATED_DATE, ISACCOUNTACTIVE, ACCOUNT_TYPE)"
-                            + "VALUES (account_id_seq.NEXTVAL,?,?,?,?,?)";
-                    pstmt = GetConn.getPreparedStatement(sql);
-                    pstmt.setString(1, nric);
-                    pstmt.setInt(2, 0);
-                    pstmt.setDate(3, Date.valueOf(LocalDate.now()));
-                    pstmt.setInt(4, 1);
-                    pstmt.setString(5, acctType);
-                    int res = pstmt.executeUpdate();
-                    System.out.println(res + " records inserted");
-
-                    sql = "SELECT * FROM ACCOUNT WHERE nric = upper(?) AND ACCOUNT_TYPE = ?";
+            
+            if (createAcc) {
+                try {
+                    System.out.println(acctType);
+                    sql = "SELECT * FROM ACCOUNT WHERE nric = upper(?) AND ACCOUNT_TYPE =?";
                     pstmt = GetConn.getPreparedStatement(sql);
                     pstmt.setString(1, nric);
                     pstmt.setString(2, acctType);
                     resultSet = pstmt.executeQuery();
-                    resultSet.next();
-                    System.out.println(acctType + " Account Created! " + acctType + " ID: " + resultSet.getInt(1));
-                    GetConn.closeConn();
-                    GetConn.closeConn();
-                    GetConn.closeConn();
-                    
-                    isExit = true;
-                    break;
-                } else {
-                    System.out.println(acctType + " Account exists already!");
-                    System.out.print(acctType + " Account ID: ");
-                    System.out.println(resultSet.getInt(1));
-                    GetConn.closeConn();
-                    break;
+
+                    if (!resultSet.next()) {
+                        sql = "INSERT INTO ACCOUNT(ACCOUNT_ID, NRIC, ACCOUNT_BALANCE, ACCOUNT_CREATED_DATE, ISACCOUNTACTIVE, ACCOUNT_TYPE)"
+                                + "VALUES (account_id_seq.NEXTVAL,?,?,?,?,?)";
+                        pstmt = GetConn.getPreparedStatement(sql);
+                        pstmt.setString(1, nric);
+                        pstmt.setInt(2, 0);
+                        pstmt.setDate(3, Date.valueOf(LocalDate.now()));
+                        pstmt.setInt(4, 1);
+                        pstmt.setString(5, acctType);
+                        int res = pstmt.executeUpdate();
+                        System.out.println(res + " records inserted");
+
+                        sql = "SELECT * FROM ACCOUNT WHERE nric = upper(?) AND ACCOUNT_TYPE = ?";
+                        pstmt = GetConn.getPreparedStatement(sql);
+                        pstmt.setString(1, nric);
+                        pstmt.setString(2, acctType);
+                        resultSet = pstmt.executeQuery();
+                        resultSet.next();
+                        System.out.println(acctType + " Account Created! " + acctType + " ID: " + resultSet.getInt(1));
+                        GetConn.closeConn();
+                        GetConn.closeConn();
+                        GetConn.closeConn();
+
+                        isExit = true;
+                        break;
+                    } else {
+                        System.out.println(acctType + " Account exists already!");
+                        System.out.print(acctType + " Account ID: ");
+                        System.out.println(resultSet.getInt(1));
+                        GetConn.closeConn();
+                        break;
+                    }
+                } catch (SQLException e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
                 }
-            } catch (SQLException e) {
-                // TODO: handle exception
-                e.printStackTrace();
             }
             isExit = true;
 
